870cc72bf8ca412b7df07ad87980bf23ba6ae6fb
Nkiriobasi <obasinkiri@gmail.com>
2022-09-03 08:33:27 +0100

    added the speakers section

---
+++ src/container/Speakers/Speakers.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Speakers.scss'
+
+const Speakers = () => {
+  return (
+    <section className='section'>
+        <div className="container">
+            <h2 className="speakers-h2">Speakers & Instructors</h2>
+
+            <div className="speakers-content"></div>
+        </div>
+    </section>
+  );
+}
+
+export default Speakers;
